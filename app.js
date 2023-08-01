27e0f4f0596872b9e1b2ea2fb53de59ace3b71d6
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-08-01 19:16:16 +0100

    Added styling and views for single and multiplayer

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static('public'));
 
 const indexRouter = require('./routes/index.js');
 const gameRouter = require('./routes/game.js');
